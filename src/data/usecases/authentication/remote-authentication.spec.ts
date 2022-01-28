ac2f88c32480ba4ca5627b1a0cb89594ab974c6b
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-01-27 23:09:56 -0300

    test: ensure HttpPostClient returns UnexpectedError if body is empty

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -77,6 +77,16 @@ describe('RemoteAuthentication', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
+  test('Should throw UnexpectedError if HttpPostClient returns 200 but body is empty', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.OK,
+      body: undefined
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
   test('Should return an AccountModel if HttpPostClient returns 200', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     const httpResult = mockAccountModel()
