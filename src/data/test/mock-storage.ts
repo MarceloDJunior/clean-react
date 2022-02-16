eba5fbc919c87a313ee1123abec7b3ccc0bf2097
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-02-15 22:38:13 -0300

    refactor: rename SetStorageSpy to SetStorageMock

--- src/data/test/mock-storage.ts
+++ src/data/test/mock-storage.ts
@@ -1,12 +1,12 @@
 import { SetStorage } from '@/data/protocols/cache/set-storage'
 
-export class SetStorageSpy implements SetStorage {
+export class SetStorageMock implements SetStorage {
   key?: string
   value?: string
 
-  async set (key: string, value: string): Promise<void> {
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async set(key: string, value: string): Promise<void> {
     this.key = key
     this.value = value
-    return Promise.resolve()
   }
 }
