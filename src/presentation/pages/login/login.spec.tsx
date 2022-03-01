9635809ce43b5cf014bf4a244cead1992ab022c8
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-03-01 17:44:59 -0300

    test: ensure Login present error if SaveAccessToken throws

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -186,6 +186,15 @@ describe('Login Page', () => {
     expect(history.location.pathname).toBe('/')
   })
 
+  test('Should pesent error if SaveAccessToken fails', async () => {
+    const { sut, saveAccessTokenMock } = makeSut()
+    const error = new InvalidCredentialsError()
+    jest.spyOn(saveAccessTokenMock, 'save').mockReturnValueOnce(Promise.reject(error))
+    await simulateValidSubmit(sut)
+    testElementText(sut, 'main-error', error.message)
+    testErrorWrapChildCount(sut, 1)
+  })
+
   test('Should go to signup page', () => {
     const { sut } = makeSut()
     const register = sut.getByTestId('signup')
