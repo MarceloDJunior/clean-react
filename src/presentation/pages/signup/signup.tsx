6f61d8217389e0a6979537ffdd0a8f665b4d20e1
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-04-01 17:48:01 -0300

    feat: ensure SignUp change name status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,18 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
 import FormContext, { formInitialState } from '@/presentation/contexts/form/form-context'
+import { Validation } from '@/presentation/protocols/validation'
 
 import styles from './signup-styles.scss'
 
-export const SignUp: React.FC = () => {
-  const [state, _] = useState({
+type Props = {
+  validation?: Validation
+}
+
+export const SignUp: React.FC<Props> = ({ validation }) => {
+  const [state, setState] = useState({
     ...formInitialState,
-    nameError: 'Campo obrigatório',
-    emailError: 'Campo obrigatório',
-    passwordError: 'Campo obrigatório',
-    passwordConfirmation: 'Campo obrigatório',
+    name: '',
+    nameError: '',
+    email: '',
+    emailError: '',
+    password: '',
+    passwordError: '',
+    passwordConfirmation: '',
+    passwordConfirmationError: '',
   })
+
+  useEffect(() => {
+    setState({
+      ...state,
+      nameError: validation?.validate('name', state.name) || '',
+      emailError: validation?.validate('email', state.email) || '',
+      passwordError: validation?.validate('password', state.password) || '',
+      passwordConfirmationError:
+        validation?.validate('passwordConfirmation', state.passwordConfirmation) || '',
+    })
+  }, [state.name, state.email, state.password, state.passwordConfirmation])
 
   return (
     <div className={styles.signup}>
@@ -20,24 +40,33 @@ export const SignUp: React.FC = () => {
       <FormContext.Provider value={state}>
         <form className={styles.form}>
           <h2>Criar conta</h2>
-          <Input type="text" name="name" placeholder="Digite seu nome" error={state.nameError} />
+          <Input
+            type="text"
+            name="name"
+            placeholder="Digite seu nome"
+            error={state.nameError}
+            onChange={event => setState({ ...state, name: event.target.value })}
+          />
           <Input
             type="email"
             name="email"
             placeholder="Digite seu e-mail"
             error={state.emailError}
+            onChange={event => setState({ ...state, email: event.target.value })}
           />
           <Input
             type="password"
             name="password"
             placeholder="Digite sua senha"
-            error={state.passwordConfirmation}
+            error={state.passwordError}
+            onChange={event => setState({ ...state, password: event.target.value })}
           />
           <Input
             type="password"
             name="passwordConfirmation"
             placeholder="Digite sua senha"
-            error={state.nameError}
+            error={state.passwordConfirmationError}
+            onChange={event => setState({ ...state, passwordConfirmation: event.target.value })}
           />
           <button type="submit" data-testid="submit" disabled className={styles.submit}>
             Entrar
