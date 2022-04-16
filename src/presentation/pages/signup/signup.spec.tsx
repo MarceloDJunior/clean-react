2540cf1d1647d10c88c4f1c5ea45a18b4abd0227
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-04-16 17:50:44 -0300

    test: ensure SignUp present error if SaveAccessToken fail

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -181,4 +181,13 @@ describe('SignUp Page', () => {
     expect(history.index).toBe(0)
     expect(history.location.pathname).toBe('/')
   })
+
+  test('Should present error if SaveAccessToken fails', async () => {
+    const { sut, saveAccessTokenMock } = makeSut()
+    const error = new EmailInUseError()
+    jest.spyOn(saveAccessTokenMock, 'save').mockRejectedValueOnce(error)
+    await simulateValidSubmit(sut)
+    Helper.testElementText(sut, 'main-error', error.message)
+    Helper.testChildCount(sut, 'error-wrap', 1)
+  })
 })
