2922ab4cb3dc84c46916a6b0e145769285cf7dd7
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-04-29 17:47:13 -0300

    refactor: change test to use faker

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -86,8 +86,8 @@ describe('Login', () => {
         invalidProperty: faker.datatype.uuid(),
       },
     })
-    cy.getByTestId('email').focus().type('mango@gmail.com')
-    cy.getByTestId('password').focus().type('12345')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
     cy.getByTestId('submit').click()
     cy.getByTestId('spinner').should('not.exist')
     cy.getByTestId('main-error').should(
