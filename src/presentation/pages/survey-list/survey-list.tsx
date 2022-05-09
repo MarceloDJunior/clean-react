771c5809dfaa73c8b6a0c0346e2a86e256fc9db1
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-05-09 18:49:13 -0300

    refactor: move Icon to its own file

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { Footer, Header } from '@/presentation/components'
+import { Footer, Header, Icon } from '@/presentation/components'
+import { IconName } from '@/presentation/components/icon/icon'
 
 import styles from './survey-list-styles.scss'
 
@@ -13,12 +14,7 @@ export const SurveyList: React.FC = () => {
         <ul>
           <li>
             <div className={styles.surveyContent}>
-              <div className={[styles.iconWrap, styles.green].join(' ')}>
-                <img
-                  className={styles.icon}
-                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII="
-                />
-              </div>
+              <Icon iconName={IconName.THUMB_UP} className={styles.iconWrap} />
               <time>
                 <span className={styles.day}>22</span>
                 <span className={styles.month}>fev</span>
